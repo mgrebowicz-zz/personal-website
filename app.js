593a68b172ae7feb9ae2d5c5f23124641b075c64
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-04-06 20:33:26 -0700

    style layout

---
+++ app.js
@@ -0,0 +1,5 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
