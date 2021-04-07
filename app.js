1b976b38140e6965728fc817096ccc422fa0fe99
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-04-06 21:13:10 -0700

    add cached element references, add landing page animation

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+/*----- cached element references -----*/
+
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
 const logo = document.querySelector('#logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
+
+const tl = new TimelineMax();
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+tl.fromTo(hero, 1, { height: '0%' }, { height: '80%', ease: Power2.easeInOut })
+    .fromTo(hero, 1.2, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
+    .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
+    .fromTo(logo, 1.2, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=1.2")
