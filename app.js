e6ca903936959c40dcde73146df9cd4397d4db1d
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-04-07 11:42:36 -0700

    add fullpage scroll animation

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const tl = new TimelineMax();
 
 /*----- functions -----*/
 
+new fullpage('#fullpage', {
+    autoSrcolling: true,
+    navigation: true
+})
+
 tl.fromTo(hero, 1, { height: '0%' }, { height: '80%', ease: Power2.easeInOut })
     .fromTo(hero, 1.2, { width: '100%' }, { width: '80%', ease: Power2.easeInOut })
     .fromTo(slider, 1.2, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
